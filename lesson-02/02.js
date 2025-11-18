49ed8d303715c1a234913dad20354ea16bf1fa9e
Anton Ilchenko <anton56.234@gmail.com>
2025-11-19 00:15:10 +0500

    L2FinalUpdated

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,7 +9,7 @@
 
 let dice1 = Math.floor(Math.random() * 6) + 1
 let dice2 = Math.floor(Math.random() * 6) + 1
-let isWinningDouble = dice1 > 3 && dice2 > 3
+let isWinningDouble = (dice1 > 3 && dice2 > 3) && (dice1=dice2)
 
 
 console.log('Первый бросок: ' + dice1)
